5a869de03ee957deabc0a2b6d757d16aaf84686e
oskery <oskar.lyrstrand@gmail.com>
2021-09-14 13:38:07 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import { Ground, Player, Cube } from './components/'
 function App() {
   return (
     <Canvas shadows colorManagement camera={{ fov: 45 }}>
-      <Sky sunPosition={[100, 20, 100]} />
+      <Sky sunPosition={[100, 10, 100]} />
       <ambientLight intensity={0.3} />
-      <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
+      <pointLight castShadow intensity={2.8} position={[100, 100, 100]} />
       <Physics gravity={[0, -30, 0]}>
         <Cube position={[0, 1, -10]} />
         <Ground />
